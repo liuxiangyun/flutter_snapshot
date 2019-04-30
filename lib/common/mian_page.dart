66f321b3b80ca5294969a1c191fb0859aad5e0de
liuxiangyun <1147814156@qq.com>
2019-05-01 01:44:57 +0800

    drawer layout

--- lib/common/mian_page.dart
+++ lib/common/mian_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_snapshot/style/text_style.dart';
 
 class MainPage extends StatelessWidget {
   @override
@@ -126,18 +127,52 @@ class _DrawerHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 230.0,
+      height: 240.0,
       width: 305.0,
-      padding: const EdgeInsetsDirectional.fromSTEB(16.0, 35.0, 16.0, 16.0),
+      padding: const EdgeInsetsDirectional.fromSTEB(16.0, 45.0, 16.0, 16.0),
       color: Theme.of(context).primaryColor,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          CircleAvatar(
-            backgroundImage: AssetImage("images/ic_user_avatar.jpg"),
-            radius: 50,
+          Row(
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage("images/ic_user_avatar.jpg"),
+                radius: 50,
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(25.0, 0.0, 0.0, 0.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "提莫队长",
+                      style: TextStyle(color: Colors.white, fontSize: 22.0),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+                      child: Text(
+                        "天蝎座",
+                        style: TextStyle(color: Colors.white, fontSize: 16.0),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
-          Text("我是你们尊敬的提莫队长，噩梦人机再次来袭，享受被我支配的恐惧吧。。。哈哈哈。。。哈哈哈。。。哈哈哈。。。")
+          Container(
+            child: Text(
+              "我是你们尊敬的提莫队长，噩梦人机再次来袭，享受被我支配的恐惧吧。。。哈哈哈。。。哈哈哈。。。哈哈哈。。。",
+              style: TextStyle(color: Colors.white),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            ),
+            margin: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+          )
         ],
       ),
     );
@@ -147,7 +182,42 @@ class _DrawerHeader extends StatelessWidget {
 class _DrawerFooter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return ListView(
+      children: <Widget>[
+        _DrawerListTile("images/ic_wallet.png", "我的钱包"),
+        _DrawerListTile("images/ic_reward.png", "赞赏"),
+        _DrawerListTile("images/ic_collect.png", "关注"),
+        _DrawerListTile("images/ic_task.png", "任务"),
+        _DrawerListTile("images/ic_browsing_history.png", "浏览记录"),
+        _DrawerListTile("images/ic_setting.png", "设置"),
+      ],
+    );
+  }
+}
+
+class _DrawerListTile extends StatelessWidget {
+  final String assetName;
+  final String title;
+
+  _DrawerListTile(this.assetName, this.title, {Key key})
+      : assert(assetName != null),
+        assert(title != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.asset(
+        assetName,
+        color: greyTextColorPrimary,
+        width: 33.0,
+        height: 33.0,
+      ),
+      title: Text(
+        title,
+        style: greyTextStylePrimary,
+      ),
+    );
   }
 }
 
---
+++ lib/style/text_style.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+///白色背景使用的灰色文字---start
+///
+///标准 87% alpha
+const greyTextColorPrimary = Color(0xDE000000);
+
+///视觉层次偏低的次要文字 54% alpha
+const greyTextColorLight = Color(0x8A000000);
+
+///像正文和标签中用于提示用户的文字，视觉层次更低 26% alpha
+const greyTextColorLightPlus = Color(0x42000000);
+
+const double fontSize_14=14.0;
+
+const greyTextStylePrimary =
+    TextStyle(color: greyTextColorPrimary, fontSize: fontSize_14);
+
+const greyTextStyleLight = TextStyle(color: greyTextColorLight, fontSize:fontSize_14);
+
+const greyTextStyleLightPlus =
+    TextStyle(color: greyTextColorLightPlus, fontSize: fontSize_14);
+
+///
+///白色背景使用的灰色文字---end
